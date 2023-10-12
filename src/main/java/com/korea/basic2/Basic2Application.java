584cac54eba507e45f3062d883bdb0084f609a0b
youjuny <youyoung2774@gmail.com>
2023-10-12 17:17:24 +0900

    자바스트링부트연습

---
+++ src/main/java/com/korea/basic2/Basic2Application.java
@@ -0,0 +1,13 @@
+package com.korea.basic2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basic2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Basic2Application.class, args);
+	}
+
+}
